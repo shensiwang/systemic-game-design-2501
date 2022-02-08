a52d70b25edd97b1c21ec58664b3a28e26fc07b6
Don25041998 <weihaodonovan@gmail.com>
2022-02-08 10:10:06 +0800

    Update Customer.cs

--- systemic-game-design-2501/Assets/RequestsScripts/Customer.cs
+++ systemic-game-design-2501/Assets/RequestsScripts/Customer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
-    // Start is called before the first frame update
     public AgeInterface Age;
     public FicklnessInterface Fickleness;
     public BaseInterface BaseNeeded;
