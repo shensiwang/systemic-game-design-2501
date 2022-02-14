ed2321b9a54993ed852e0cbfd7eba54f76d9ed74
Kwjx <72851093+Kwjx@users.noreply.github.com>
2022-02-14 23:58:18 +0800

    Added some stuff to the brewing

--- systemic-game-design-2501/Assets/BrewingScript/ReceipeManager.cs
+++ systemic-game-design-2501/Assets/BrewingScript/ReceipeManager.cs
@@ -7,11 +7,15 @@ public class ReceipeManager : MonoBehaviour
 {
     private Ingredients currentIngredient;
     public Image customCursor;
+    public Image ingredientBlocker;
 
-    public Slots[] craftingSlots;
+    public Slots baseCraftingSlots;
+    public Slots[] subCraftingSlots;
+
     public List<Ingredients> ingredientList;
-    public string[] receipeList;
-    public string[] receipeResults;
+    public string[] receipes;
+    public Ingredients[] brewingResults;
+    public string receipeResults;
 
     public void OnMouseDownIngredient(Ingredients ingredients)
     {
@@ -33,21 +37,81 @@ public class ReceipeManager : MonoBehaviour
                 Slots nearestSlot = null;
                 float shortestDistance = float.MaxValue;
 
-                foreach(Slots slot in craftingSlots)
+                if (currentIngredient.gameObject.CompareTag("SubIngredient"))
                 {
-                    float dist = Vector2.Distance(Input.mousePosition, slot.transform.position);
-                    if(dist < shortestDistance)
+                    foreach (Slots slot in subCraftingSlots)
                     {
-                        shortestDistance = dist;
-                        nearestSlot = slot;
+                        float dist = Vector2.Distance(Input.mousePosition, slot.transform.position);
+                        if (dist < shortestDistance)
+                        {
+                            shortestDistance = dist;
+                            nearestSlot = slot;
+                        }
                     }
                 }
 
+                else if (currentIngredient.gameObject.CompareTag("BaseIngredient"))
+                {
+                    float dist = Vector2.Distance(Input.mousePosition, baseCraftingSlots.transform.position);
+                    if (dist < shortestDistance)
+                    {
+                        nearestSlot = baseCraftingSlots;
+                        ingredientBlocker.gameObject.SetActive(true);
+                    }
+                  
+                }
                 nearestSlot.gameObject.SetActive(true);
                 nearestSlot.GetComponent<Image>().sprite = currentIngredient.GetComponent<Image>().sprite;
                 nearestSlot.ingredients = currentIngredient;
+                ingredientList[nearestSlot.index] = currentIngredient;
+
                 currentIngredient = null;
+                Debug.Log(ingredientList[nearestSlot.index]);
+                CheckForCreatedReceipes();
             }
+        }
+    }
+
+    public void CheckForCreatedReceipes()
+    {
+        string currentReceipeString = "";
+        foreach(Ingredients ingredients in ingredientList)
+        {
+            if(ingredients != null)
+            {
+                currentReceipeString += ingredients.ingredientName;
+            }
+            else
+            {
+                currentReceipeString += "null";
+            }
+        }
+
+        for(int i = 0; i < receipes.Length; i++)
+        {
+            if(receipes[i] == currentReceipeString)
+            {
+                Debug.Log(currentReceipeString);
+            }
+        }
+    }
+
+    public void ResetPot(Slots slot )
+    {
+        baseCraftingSlots.ingredients = null;
+        ingredientList[slot.index] = null;
+        CheckForCreatedReceipes();
+        ingredientBlocker.gameObject.SetActive(false);
+        Debug.Log("Gone");
+    }
+
+    public void OnMouseDown(Ingredients ingredients)
+    {
+        if(currentIngredient == null)
+        {
+            currentIngredient = ingredients;
+            customCursor.gameObject.SetActive(true);
+            customCursor.sprite = currentIngredient.GetComponent<Image>().sprite;
         }
     }
 }
