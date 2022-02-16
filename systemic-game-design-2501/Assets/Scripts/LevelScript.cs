a9388bf02ca9bef30e4e1c3be7d0746f7fe64978
cakejw <72867369+cakejw@users.noreply.github.com>
2022-02-16 13:35:32 +0800

    tmp. display customers left, spawn customer
    despawning needs customer variables to be public

--- systemic-game-design-2501/Assets/Scripts/LevelScript.cs
+++ systemic-game-design-2501/Assets/Scripts/LevelScript.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelScript : MonoBehaviour
 {
+    //public int DaysPassed;
+
+    public GameObject reviewSheet;
+
+    public Image patienceUI;
 
     public GameObject customerPrefab;
 
     public GameObject currentCustomer;
+    private Customer currentCustomerScript;
+
     public Transform customerSpawnPoint;
 
     public int totalCustomersPerDay;
+    private int numberOfCustomersLeft;
+
+    public TextMeshProUGUI numberOfCustomersLeftUI;
 
     public int customersSucceeded;
     public int customersFailed;
@@ -22,13 +34,24 @@ public class LevelScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnCustomer();
+
+       SpawnCustomer();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        numberOfCustomersLeft = totalCustomersPerDay - customersSucceeded - customersFailed;
+        numberOfCustomersLeftUI.text = "Customers left: " + numberOfCustomersLeft;
+
+        ReviewSheet();
+
+        //if(currentCustomerScript.cu)
+
+        //patienceUI.fillAmount = (currentCustomerScript.CurrentTimer / currentCustomerScript.PatienceTimer); need to make customer current timer public
+
     }
 
     private void SpawnCustomer() //spawn customer 
@@ -37,6 +60,36 @@ public class LevelScript : MonoBehaviour
         {
             currentCustomer = Instantiate(customerPrefab, customerSpawnPoint);
             customerIsPresent = true;
+
+            currentCustomerScript = currentCustomer.gameObject.GetComponent<Customer>();
         }
     }
+
+    private void DespawnCustomer()
+    {
+        if(customerIsPresent == true)
+        {
+            Destroy(currentCustomer); //or whatever code to remove the customer
+
+            
+        }
+    }
+
+    //when failed/passed, customers -1
+
+
+    //when succeed RepManager.repMaster.IncreaseRep(10)
+    //when fail RepManager.repMaster.DecreaseRep(10)
+
+    private void ReviewSheet()
+    {
+        if(numberOfCustomersLeft == 0)
+        {
+            //display reviewsheet
+
+            reviewSheet.SetActive(true);
+        }
+    }
+
+
 }
