bd09e444eaaba6b4c847a36c4d3f2b6f16cdc97f
Don25041998 <weihaodonovan@gmail.com>
2022-03-03 09:51:48 +0800

    Dialogue Manager
    Added Dialogue Manager

---
+++ systemic-game-design-2501/Assets/Scripts/RequestsScripts/DialogueManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private The_Customer CurrentCustomer;
+    public Text Dialogue;
+    public bool StartedDialogue = false;
+    private int sentenceLine = 0;
+    private List<string> CurrentCustomerScript;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(CurrentCustomer==null)
+        {
+            sentenceLine = 0;
+            Dialogue.text = "";
+            GameObject Customer = GameObject.FindGameObjectWithTag("Customer");
+            if (Customer!=null)
+            {
+                CurrentCustomer = Customer.GetComponent<The_Customer>();
+                CurrentCustomerScript = CurrentCustomer.CustomerScript;
+            }
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+
+    public void StartDialogue()
+    {
+        Dialogue.text = CurrentCustomerScript[sentenceLine];
+        StartedDialogue = true;
+    }
+
+    public void NextDialogue()
+    {
+        if(sentenceLine < CurrentCustomerScript.Count-1)
+        {
+            sentenceLine++;
+            Dialogue.text = CurrentCustomerScript[sentenceLine];
+        }
+
+    }
+
+}
