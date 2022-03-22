5b9e01744cf5a924c8bed50fbf392518158006ae
MarshmallowJin <charmstars@gmail.com>
2022-03-22 22:07:08 +0800

    main menu done up

---
+++ systemic-game-design-2501/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject quitGameComfirmation;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        quitGameComfirmation.SetActive(true);
+    }
+
+    public void DontQuitGame()
+    {
+        quitGameComfirmation.SetActive(false);
+    }
+
+    public void QuitGameConfirmation()
+    {
+        Application.Quit();
+    }
+}
