148527c5ebcd1e194d96839ca9fbfae79d058ea2
MarshmallowJin <charmstars@gmail.com>
2022-03-28 19:50:13 +0800

    Implementing intro cutscene

--- systemic-game-design-2501/Assets/Scripts/Menu/MainMenu.cs
+++ systemic-game-design-2501/Assets/Scripts/Menu/MainMenu.cs
@@ -6,13 +6,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject quitGameComfirmation;
+    public GameObject fadeIn;
     public Loadscreen Load;
 
-    public void Update()
-    {
-        
-    }
+    //Cutscenes Variables
+    [Header("Cutscenes")]
+    public Animator fadeAnim;
+    public GameObject cutscene1;
+    public GameObject dialogueText;
+    public GameObject cutsceneStore;
+    public Animator cutsceneAnim;
+    public Animator dialogueAnim;
 
+    //Main Menu Transitions
     public void PlayGame()
     {
         Load.playButtonPresed();
@@ -31,5 +37,107 @@ public class MainMenu : MonoBehaviour
     public void QuitGameConfirmation()
     {
         Application.Quit();
+    }
+
+
+    //CUTSCENE FUNCTIONS - Pls dont mess with this 
+    public void FadeToBlack()
+    {
+        fadeIn.SetActive(true);
+        fadeAnim.SetTrigger("StartCutscene");
+
+        Invoke("Cutscene1", 2f);
+    }
+
+    public void Cutscene1()
+    {
+        cutscene1.SetActive(true);
+        Invoke("CutsceneBar", 0.6f);
+    }
+
+    public void CutsceneBar()
+    {
+        cutsceneAnim.SetTrigger("CutsceneBar");
+        Invoke("BlueFaction", 2f);
+    }
+
+    public void BlueFaction()
+    {
+        cutsceneAnim.SetTrigger("BlueFactionIntro");
+        Invoke("Dialogue1", 1f);
+    }
+
+    public void Dialogue1()
+    {
+        dialogueText.SetActive(true);
+        Invoke("Dialogue1Fade", 4f);
+    }
+
+    public void Dialogue1Fade()
+    {
+        dialogueAnim.SetTrigger("Dialogue1Fade");
+        Invoke("BlueFactionOut", 1f);
+    }
+
+    public void BlueFactionOut()
+    {
+        cutsceneAnim.SetTrigger("BlueFactionOut");
+        Invoke("RedFactionIntro", 1f);
+    }
+
+    public void RedFactionIntro()
+    {
+        cutsceneAnim.SetTrigger("RedFactionIntro");
+        Invoke("Dialogue2", 1f);
+    }
+
+    public void Dialogue2()
+    {
+        dialogueAnim.SetTrigger("Dialogue2");
+        Invoke("Dialogue2Fade", 4f);
+    }
+
+    public void Dialogue2Fade()
+    {
+        dialogueAnim.SetTrigger("Dialogue2Fade");
+        cutsceneAnim.SetTrigger("FadeToStore");
+        Invoke("ShopIntro", 4f);
+    }
+
+    public void ShopIntro()
+    {
+        cutsceneStore.SetActive(true);
+        cutsceneAnim.SetTrigger("StoreStart");
+        Invoke("Dialogue3", 2.5f);
+    }
+
+    public void Dialogue3()
+    {
+        dialogueAnim.SetTrigger("Dialogue3");
+        Invoke("Dialogue3Fade", 3.5f);
+    }
+
+    public void Dialogue3Fade()
+    {
+        dialogueAnim.SetTrigger("Dialogue3Fade");
+        Invoke("Dialogue4", 1.5f);
+    }
+
+    public void Dialogue4()
+    {
+        dialogueAnim.SetTrigger("Dialogue4");
+        Invoke("Dialogue4Fade", 3.5f);
+    }
+
+    public void Dialogue4Fade()
+    {
+        dialogueAnim.SetTrigger("Dialogue4Fade");
+        Invoke("FadeToGame", 9f);
+    }
+
+    public void FadeToGame()
+    {
+        fadeAnim.SetTrigger("StartGame");
+        Invoke("PlayGame", 1f);
     }
 }
