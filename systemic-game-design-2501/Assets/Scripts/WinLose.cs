7252d4387a3317f6d0ae8376e123697f0d08c278
cakejw <kekjiawei@gmail.com>
2022-04-03 21:59:18 +0800

    lose description

--- systemic-game-design-2501/Assets/Scripts/WinLose.cs
+++ systemic-game-design-2501/Assets/Scripts/WinLose.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WinLose : MonoBehaviour
 {
 
     public GameObject winMenu;
     public GameObject loseMenu;
+
+
+    public TextMeshProUGUI loseDescription;
+
+    public string loseDescriptionTextAudreyWin;
+    public string loseDescriptionTextBetramWin;
+
+    public Faction factionData;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +32,20 @@ public class WinLose : MonoBehaviour
     public void Win()
     {
         winMenu.SetActive(true);
+        
     }
 
     public void Lose()
     {
         loseMenu.SetActive(true);
+
+        if (factionData.winningFaction == "A")
+        {
+            loseDescription.text = loseDescriptionTextAudreyWin;
+        }
+        else
+        {
+            loseDescription.text = loseDescriptionTextBetramWin;
+        }
     }
 }
